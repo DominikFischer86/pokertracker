c8597dfca4f85fd209b1ae7fede10e4d693eded0
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2021-09-12 21:03:49 +0200

    Add additional api methods

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -5,7 +5,7 @@ import ReviewsController from "./reviews.controller.js"
 const router = express.Router()
 
 router.route("/").get(RestaurantsController.apiGetRestaurants)
-router.route("/id/:id").get(RestaurantsController.apiGetRestaurantbyId)
+router.route("/id/:id").get(RestaurantsController.apiGetRestaurantById)
 router.route("/cuisines").get(RestaurantsController.apiGetRestaurantCuisines)
 
 router
