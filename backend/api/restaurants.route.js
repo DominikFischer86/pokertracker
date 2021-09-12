cb7bf240537a3e7b16c2acbfbb6bd286e0245166
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2021-09-12 20:41:06 +0200

    Create CRUD methods to mongodb atlas server

---
+++ backend/api/restaurants.route.js
@@ -0,0 +1,18 @@
+import express from "express"
+import RestaurantsController from "../api/restaurants.controller.js"
+import ReviewsController from "./reviews.controller.js"
+
+const router = express.Router()
+
+router.route("/").get(RestaurantsController.apiGetRestaurants)
+router.route("/id/:id").get(RestaurantsController.apiGetRestaurantbyId)
+router.route("/cuisines").get(RestaurantsController.apiGetRestaurantCuisines)
+
+router
+    .route("/review")
+    .post(ReviewsController.apiPostReview)
+    .put(ReviewsController.apiUpdateReview)
+    .delete(ReviewsController.apiDeleteReview)
+
+
+export default router
