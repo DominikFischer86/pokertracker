f05ecb8d5206c947756184f7e60ad1f3e0d226a9
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2021-09-21 23:18:43 +0200

    Add switch for placements and result presentation

--- frontend/src/components/ImportPage/ImportPage.js
+++ frontend/src/components/ImportPage/ImportPage.js
@@ -2,7 +2,8 @@
 import React, { useState } from "react"
 import { Chip } from "@react-md/chip";
 import { Button } from "react-md"
-import { TextField } from "@react-md/form";
+import { TextField, Switch } from "@react-md/form";
+import { TabsManager, Tabs, TabPanels, TabPanel } from "@react-md/tabs";
 import { FaEye, FaUndo, FaCheck } from 'react-icons/fa';
 import {
     ExpansionList,
@@ -18,6 +19,7 @@ const ImportPage = () => {
     const [tournamentMap, setTournamentMap] = useState({})
     const [playerAmountCreator, setPlayerAmountCreator] = useState([])
 
+    const [skipPlacements, setSkipPlacements] = useState(false)
     const [isReadyToCreate, setIsReadyToCreate] = useState(false)
     const [isReadyToInput, setIsReadyToInput] = useState(false)
     const [isReadyToSubmit, setIsReadyToSubmit] = useState(false)
@@ -30,6 +32,12 @@ const ImportPage = () => {
       });
     
       const [panel1Props, panel2Props, panel3Props] = panels;
+
+    const tabs = ["Table", "JSON"];
+    
+    const handleSwitch = () => {
+        setSkipPlacements(!skipPlacements)
+    }
 
     const handleFormChange = event => {
         const { name, value } = event.target
@@ -70,17 +78,28 @@ const ImportPage = () => {
 
     const submitGeneralFormData = e => {
         e.preventDefault()
-        const playerAmount = parseFloat(Object.values(form.playerAmount).join().replace(',',''))
 
-        playerAmountCreator.splice(0, playerAmountCreator.length)
-        for (let i = 1; i <= playerAmount; i++){
-            playerAmountCreator.push(i)
+        if (!skipPlacements){
+            const playerAmount = parseFloat(Object.values(form.playerAmount).join().replace(',',''))
+
+            playerAmountCreator.splice(0, playerAmountCreator.length)
+            for (let i = 1; i <= playerAmount; i++){
+                playerAmountCreator.push(i)
+            }
+            setPlayerAmountCreator(playerAmountCreator)
+            setIsReadyToInput(true)
+        } else {
+            
+            setTournamentMap({
+                ...form,
+                'placements': []
+            })
+            setIsReadyToSubmit(true)
         }
-        setPlayerAmountCreator(playerAmountCreator)
-        setIsReadyToInput(true)
+        
     }
 
-    const submitForms = () => {
+    const convertData = () => {
         let newPlacement = []
         let newPlacementMap = []
         let arr = []
@@ -110,8 +129,7 @@ const ImportPage = () => {
         setIsReadyToSubmit(true)
     }
 
-    console.log(tournamentMap)
-    console.log(JSON.stringify(tournamentMap))
+    // console.log(tournamentMap)
 
     return (
         <div>
@@ -140,17 +158,9 @@ const ImportPage = () => {
                                         buttonType="icon" 
                                         aria-label="Preview"
                                         disabled={!isReadyToInput}
+                                        onClick={convertData}
                                     >
                                         <FaEye />
-                                    </Button>
-                                    <Button 
-                                        theme="primary" 
-                                        buttonType="icon" 
-                                        aria-label="Submit"
-                                        disabled={!isReadyToInput}
-                                        onClick={submitForms}
-                                    >
-                                        <FaCheck />
                                     </Button>
                                 </div>
                             </div>
@@ -158,6 +168,13 @@ const ImportPage = () => {
                             <form className="col-lg-3" autoComplete="off" onSubmit={submitGeneralFormData}>
                                 <div className="border rounded p-2">
                                 <h4>General Data</h4>
+                                <hr />
+                                    <Switch 
+                                        id="type-switcher" 
+                                        name="type-switcher" 
+                                        label="Skip placements" 
+                                        onChange={handleSwitch}
+                                    />
                                 <hr />
                                 {formFields.map((field, index) => {
                                     const [id, text, placeholder] = field
@@ -178,7 +195,9 @@ const ImportPage = () => {
                                 })}
                                 <hr />
                                     <div className="generalDataFormSubmitButton">
-                                        <Button type="submit" theme="warning" themeType="contained" disabled={!isReadyToCreate}>Enter players</Button>
+                                        <Button type="submit" theme="warning" themeType="contained" disabled={!isReadyToCreate}>
+                                            {!skipPlacements ? "Enter players" : "Preview Data"}
+                                        </Button>
                                     </div>
                                 </div>                            
                             </form>
@@ -187,8 +206,11 @@ const ImportPage = () => {
                                 <form className="border rounded p-2" autoComplete="off">
                                     <h4>Players/Placements</h4>
                                     <hr />
-                                    {!isReadyToInput &&
-                                    <div><p><strong>Please fill in General Data before enabling player/placements input.</strong></p></div>
+                                    {!isReadyToInput && !skipPlacements &&
+                                        <div><p><strong>Please fill in General Data before enabling player/placements input.</strong></p></div>
+                                    }
+                                    {skipPlacements &&
+                                        <div><p><strong>No placement input required. Useful for tournaments with a lot of players.</strong></p></div>
                                     }
                                     {isReadyToInput && playerAmountCreator.map(item => {
                                         return (
@@ -233,38 +255,60 @@ const ImportPage = () => {
                     </div>
                 </ExpansionPanel>
             {/* PREVIEW TABLE */}
-            <ExpansionPanel {...panel3Props} header="Vorschau" className="mt-2">
+            <ExpansionPanel {...panel3Props} header="Preview" className="mt-2">
                 <div className="border mt-2 p-2">
-                    <h3>Vorschau:</h3>
+                    <div className="formNav">
+                        <h3>Preview:</h3>
+                        <div>
+                            <Button 
+                                theme="primary" 
+                                buttonType="icon" 
+                                aria-label="Submit"
+                                disabled={!isReadyToInput}
+                                onClick={convertData}
+                            >
+                                <FaCheck />
+                            </Button>
+                        </div>
+                    </div>
                     {isReadyToSubmit &&
-                    <table className="previewTable border">
-                        <thead>
-                            <tr>
-                                {formFields.map((field,index) => {
-                                    return (
-                                        <th key={index}>{field[1]}</th>
-                                    )
-                                })}
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>{form.tournamentId}</td>
-                                <td>${form.buyIn}</td>
-                                <td>${form.rake}</td>
-                                <td>{form.playerAmount}</td>
-                                <td>${form.prizePool}</td>
-                                <td>{form.startDate}</td>
-                                <td>{form.startTime} ET</td>
-                                <td>{form.finalPosition}/{form.playerAmount}</td>
-                                <td>${form.prizeMoney}</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <TabsManager tabs={tabs} tabsId="tournament-output">
+                        <Tabs />
+                        <TabPanels>
+                            <TabPanel>
+                                <table className="previewTable border">
+                                    <thead>
+                                        <tr>
+                                            {formFields.map((field,index) => {
+                                                return (
+                                                    <th key={index}>{field[1]}</th>
+                                                )
+                                            })}
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>{form.tournamentId}</td>
+                                            <td>${form.buyIn}</td>
+                                            <td>${form.rake}</td>
+                                            <td>{form.playerAmount}</td>
+                                            <td>${form.prizePool}</td>
+                                            <td>{form.startDate}</td>
+                                            <td>{form.startTime} ET</td>
+                                            <td>{form.finalPosition}/{form.playerAmount}</td>
+                                            <td>${form.prizeMoney}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </TabPanel>
+                            <TabPanel>
+                                <code className="border">{JSON.stringify(tournamentMap)}</code>
+                            </TabPanel>
+                        </TabPanels>
+                    </TabsManager>
                      }
                 </div>
-               
-            </ExpansionPanel>
+              </ExpansionPanel>
         </ExpansionList>
     </div>
     )
