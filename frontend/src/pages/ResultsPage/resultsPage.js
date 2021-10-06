23dfa08c2e88f96a74dd900d3aaec418456514a6
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2021-10-06 02:48:57 +0200

    Add toggle to switch between tables and graphs

--- frontend/src/pages/ResultsPage/resultsPage.js
+++ frontend/src/pages/ResultsPage/resultsPage.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
+import { Switch } from "@react-md/form"
 
 import ResultsTable from "./components/ResultsTable"
+import ResultsGraph from "./components/ResultsGraph"
 
 const ResultsPage = () => {
     const [tournaments, setTournaments] = useState([])
+    const [viewToggle, setViewToggle] = useState(true)
     const [isLoading, setIsLoading] = useState(true)
+
+    const handleSwitch = () => {
+        setViewToggle(!viewToggle)
+    }
 
     const url = "http://localhost:3001/results/"    
 
@@ -26,7 +33,7 @@ const ResultsPage = () => {
         return () => {
             isFetched = true
         }        
-    }, [tournaments])
+    }, [])
 
     const onDelete = id => {
         if (confirm(`Do you really want to remove tournament #${id}`)){
@@ -36,11 +43,30 @@ const ResultsPage = () => {
     }
 
     return (
-        <ResultsTable
-            tournaments={tournaments}
-            isLoading={isLoading}
-            onDelete={onDelete}
-        />
+        <div>
+            <div className="ResultsTitleContainer">
+                <h2>Tournament Results ({tournaments.length})</h2>
+                <Switch 
+                    id="view-switcher" 
+                    name="view-switcher" 
+                    label={!viewToggle ? "View tables" : "View graph"}
+                    onChange={handleSwitch} 
+                />
+            </div>
+            <hr />
+            {viewToggle 
+            ? <ResultsTable
+                tournaments={tournaments}
+                isLoading={isLoading}
+                onDelete={onDelete}
+            />
+            : <ResultsGraph 
+                tournaments={tournaments} 
+                isLoading={isLoading} 
+            />
+            }
+        </div>
+        
     )
 }
 
