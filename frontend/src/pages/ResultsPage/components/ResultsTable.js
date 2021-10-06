45c56ff8c62342af135abb1b089da94b122459b7
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2021-10-06 02:48:39 +0200

    Remove title from Table

--- frontend/src/pages/ResultsPage/components/ResultsTable.js
+++ frontend/src/pages/ResultsPage/components/ResultsTable.js
@@ -5,60 +5,54 @@ import { FaTrashAlt } from "react-icons/fa"
 const ResultsTable = ({isLoading, tournaments, onDelete}) => {
 
     return (
-        <div>
-            <h2>Tournament Results</h2>
-            <hr />
-            <div className="row">
-                <div>
-                {isLoading && <p>Loading...</p>}
-                {!isLoading &&
-                    <table className="resultsTable">
-                        <thead>
-                            <tr>
-                                <th>Tournament Id</th>
-                                <th>Buy-In</th>
-                                <th>Rake</th>
-                                <th>Player Amount</th>
-                                <th>Prize Pool</th>
-                                <th>Start Date</th>
-                                <th>Start Time</th>
-                                <th>Final Position</th>
-                                <th>Player Prize Money</th>
-                                <th>Actions</th>
+        <div className="row">
+            {isLoading && <p>Loading...</p>}
+            {!isLoading &&
+                <table className="resultsTable">
+                    <thead>
+                        <tr>
+                            <th>Tournament Id</th>
+                            <th>Buy-In</th>
+                            <th>Rake</th>
+                            <th>Player Amount</th>
+                            <th>Prize Pool</th>
+                            <th>Start Date</th>
+                            <th>Start Time</th>
+                            <th>Final Position</th>
+                            <th>Player Prize Money</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>                            
+                        {tournaments.map((tournament, i) => {
+                            return (
+                            <tr key={i} style={
+                                tournament.playerPrizeMoney > 0 
+                                    ? {color: "rgb(0,150,0)", fontWeight: "500"} 
+                                    : null                                            
+                                }>
+                                <td><a href={`/tournament/${tournament.tournamentId}`}>{tournament.tournamentId}</a></td>
+                                <td>${tournament.buyIn}</td>
+                                <td>${tournament.rake}</td>
+                                <td>{tournament.playerAmount}</td>
+                                <td>${tournament.prizePool}</td>
+                                <td>{tournament.startDate}</td>
+                                <td>{tournament.startTime}</td>
+                                <td>{tournament.finalPosition}</td>
+                                <td>{tournament.playerPrizeMoney}</td>
+                                <td>
+                                    <FaTrashAlt 
+                                        onClick={() => onDelete(tournament.tournamentId)}
+                                        className="trash-icon"
+                                        style={{color: "red"}}
+                                    />
+                                </td>                              
                             </tr>
-                        </thead>
-                        <tbody>                            
-                            {tournaments.map((tournament, i) => {
-                                return (
-                                <tr key={i} style={
-                                    tournament.playerPrizeMoney > 0 
-                                        ? {color: "rgb(0,150,0)", fontWeight: "500"} 
-                                        : null                                            
-                                    }>
-                                    <td><a href={`/tournament/${tournament.tournamentId}`}>{tournament.tournamentId}</a></td>
-                                    <td>${tournament.buyIn}</td>
-                                    <td>${tournament.rake}</td>
-                                    <td>{tournament.playerAmount}</td>
-                                    <td>${tournament.prizePool}</td>
-                                    <td>{tournament.startDate}</td>
-                                    <td>{tournament.startTime}</td>
-                                    <td>{tournament.finalPosition}</td>
-                                    <td>{tournament.playerPrizeMoney}</td>
-                                    <td>
-                                        <FaTrashAlt 
-                                            onClick={() => onDelete(tournament.tournamentId)}
-                                            className="trash-icon"
-                                            style={{color: "red"}}
-                                        />
-                                    </td>                              
-                                </tr>
-                                )
-                            })}
-                        </tbody>
-                    </table>
-                    }
-                </div>
-            </div>
+                            )
+                        })}
+                    </tbody>
+                </table>
+                }
         </div>
     )
 }
