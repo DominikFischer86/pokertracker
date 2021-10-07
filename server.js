63898c13ae0923a71ffb056d2718e6b1d7831660
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2021-10-07 02:08:38 +0200

    Increase upload size limit

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const app = express()
 const cors = require("cors")
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
+const bodyParser = require("body-parser")
 const PORT = 3001
 
 // config
 app.use(cors())
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
 
 // connect to mongodb
 dotenv.config()
