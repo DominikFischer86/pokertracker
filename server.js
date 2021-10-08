800c025698b775332719189103c0a13d6cd5d167
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2021-10-08 23:52:35 +0200

    Remove redundant use()

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const PORT = 3001
 app.use(cors())
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true}));
-app.use(express.json())
 
 // connect to mongodb
 dotenv.config()
