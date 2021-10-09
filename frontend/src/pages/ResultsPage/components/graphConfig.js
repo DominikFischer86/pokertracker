cbb6767396cb433c1e5a893607d6a297242ed4e8
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2021-10-09 02:19:21 +0200

    Fix graph calculations

--- frontend/src/pages/ResultsPage/components/graphConfig.js
+++ frontend/src/pages/ResultsPage/components/graphConfig.js
@@ -1,17 +1,27 @@
 
 export const data = (tournaments, toggleRake) => {
-    let winnings = 0
+    let netWin = 0
+    let totalCash = 0
+    let totalRake = 0
     let newWinningData = []
     let newRakeData = []
 
-    tournaments.forEach((element, index) => {        
-        let buyIn = parseFloat((element.buyIn * (index+1)).toFixed(2))
-        let rake = parseFloat((element.rake * (index+1)).toFixed(2))
-        winnings = parseFloat((element.playerPrizeMoney + winnings).toFixed(2))
-        let profit = parseFloat((winnings - (buyIn + rake)).toFixed(2))
-        
-        let winningsObject = { "x": (index+1), "y": profit}
-        let rakeObject = {"x": (index+1), "y": rake}
+    tournaments.forEach((element, index) => {
+        let buyIn = parseFloat((element.buyIn).toFixed(2))
+        let rake = parseFloat((element.rake).toFixed(2))        
+        let winnings = parseFloat((element.playerPrizeMoney).toFixed(2))
+        totalRake = parseFloat((totalRake + rake).toFixed(2))
+        netWin = parseFloat((winnings - (buyIn + rake)).toFixed(2)) 
+        totalCash = parseFloat((netWin + totalCash).toFixed(2))
+        // console.log(index+1) DEBUGGER
+        // console.log(`%c BuyIn + Rake: ${rake + buyIn}`, "color : red")
+        // console.log(`%c NetWins: ${netWin}`, "color : orange")
+        // console.log(`%c TotalRake: ${totalRake}`, "color : blue")
+        // console.log(`%c TotalWins: ${netWin}`, "color : green")
+        // console.log("-------------------------------------")
+
+        let winningsObject = { "x": (index+1), "y": totalCash}
+        let rakeObject = {"x": (index+1), "y": totalRake}
         newWinningData.push(winningsObject)
         newRakeData.push(rakeObject)
     })
