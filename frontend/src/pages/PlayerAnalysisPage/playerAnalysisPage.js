faf063d4577c24e141555166a7f8dc5227ba4db5
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2021-10-23 06:13:01 +0200

    Set up get request on player page

--- frontend/src/pages/PlayerAnalysisPage/playerAnalysisPage.js
+++ frontend/src/pages/PlayerAnalysisPage/playerAnalysisPage.js
@@ -1,9 +1,28 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import axios from "axios"
 
 const PlayerAnalysisPage = () => {
-    return (
+    const [tournament, setTournament] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    console.log(tournament)
+
+    const url = "http://localhost:3001" + window.location.pathname
+
+    useEffect(async () => {
+        try {
+             await axios.get(url)
+                .then(res => {      
+                    setTournament(res.data[0])
+                    setIsLoading(false)          
+                })
+        } catch (e) {
+            console.log(e)
+        }
+    }, [])
+
+    return (        
         <div>
-            PlayerAnalysisPage
+            {isLoading && <div>Loading tournaments</div>}
         </div>
     )
 }
