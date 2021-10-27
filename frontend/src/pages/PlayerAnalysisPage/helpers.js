aaf6131c1078976fe8bc2345f5275b3b42c335fd
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2021-10-28 00:43:06 +0200

    Add player-analysis page

---
+++ frontend/src/pages/PlayerAnalysisPage/helpers.js
@@ -0,0 +1,45 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const createUsers = (tournaments, setIsLoading, setPlayers, heroName) => {
+    setIsLoading(true)
+    let allPlayers = []
+    let allPlayersList = []
+    let tournamentsList = []
+    let playerCountry = ""
+
+    tournaments.forEach(item => {
+        const placements = item.placements            
+        placements.forEach(placement => {
+            return allPlayers.push(placement.playerName)
+        })
+
+        allPlayersList = [...new Set(allPlayers)]
+    })
+
+    const playersMap = allPlayersList.map(player => {
+        const allTournaments = tournaments.filter(tournament => {
+            return tournament.placements.find(item => item.playerName === player)
+        })
+                
+        tournamentsList = [...new Set(allTournaments)]
+
+        tournamentsList.find(tournament => {
+            playerCountry = (tournament.placements.find(item => item.playerName === player)).playerCountry
+        })
+        
+        const newPlayerId = uuidv4()
+
+        const playerMap = [
+            [ "playerId", newPlayerId ],
+            [ "playerName", player ],
+            [ "playerCountry", playerCountry ],
+            [ "playerIsHero", player === heroName ? true : false ],
+            [ "playerTournaments", tournamentsList ]
+        ]
+
+        return {[newPlayerId]: Object.fromEntries(playerMap)}
+    })
+    
+    setPlayers(playersMap)
+    setIsLoading(false)
+}
--- frontend/src/pages/PlayerAnalysisPage/playerAnalysisPage.js
+++ frontend/src/pages/PlayerAnalysisPage/playerAnalysisPage.js
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from "react"
 import axios from "axios"
 
+import { createUsers } from "./helpers"
+
+import "./playerAnalysisPage.scss"
+
 const PlayerAnalysisPage = () => {
-    const [tournament, setTournament] = useState([])
     const [isLoading, setIsLoading] = useState(true)
-    console.log(tournament)
+    const [players, setPlayers] = useState([])
+    const [tournaments, setTournaments] = useState([])
+
+    const heroName = "KeinKönich"
 
     const url = "http://localhost:3001" + window.location.pathname
 
@@ -12,17 +18,51 @@ const PlayerAnalysisPage = () => {
         try {
              await axios.get(url)
                 .then(res => {      
-                    setTournament(res.data[0])
-                    setIsLoading(false)          
+                    setTournaments(res.data)
+                    setIsLoading(false)
                 })
         } catch (e) {
             console.log(e)
         }
     }, [])
 
+    const createUserClick = () => {
+        createUsers(tournaments, setIsLoading, setPlayers, heroName)  
+    }
+
+    const updateUserClick = () => {
+        alert("Click!")
+    }
+
     return (        
         <div>
-            {isLoading && <div>Loading tournaments</div>}
+            {isLoading && <div>Loading and creating user list...</div>}
+            {!isLoading &&
+                <div>
+                    <div className="Player_analysis_title">
+                        <h2>All Players</h2>
+                        <button style={{display: players.length > 0 ? "none" : "block"}} onClick={createUserClick}>Create user database</button>
+                        <button style={{display: players.length > 0 ? "block" : "none"}} onClick={updateUserClick}>Update user database</button>
+                    </div>
+                    <hr />
+                    <ul style={{display: "flex", flexWrap: "wrap" }}>
+                        {players.splice(1, 100).map(
+                            (player, index) => 
+                            <li 
+                                style={{
+                                    listStyleType:"none", 
+                                    border: "1px solid #000", 
+                                    margin: "5px", 
+                                    padding: "10px"
+                                }} 
+                                key={index}
+                            >
+                                #{index} {Object.values(player)[0].playerName}
+                            </li>
+                        )}
+                    </ul>
+                </div>
+            }
         </div>
     )
 }
