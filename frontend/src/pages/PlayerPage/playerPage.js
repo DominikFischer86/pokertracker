0e485cb89eb8656c74eb18812b4d4905fb2034c8
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2021-11-09 23:19:33 +0100

    Fetch tournaments and sketch needed data

--- frontend/src/pages/PlayerPage/playerPage.js
+++ frontend/src/pages/PlayerPage/playerPage.js
@@ -5,30 +5,64 @@ import axios from "axios"
 const PlayerPage = () => {
     const [isLoading, setIsLoading] = useState(true)
     const [player, setPlayer] = useState([])
-    const url = "http://localhost:3001" + window.location.pathname
+    const [allTournaments, setAllTournaments] = useState([])
 
-    useEffect(async () => {
+    const url = "http://localhost:3001" + window.location.pathname
+    const { playerCountry, playerName, playerIsHero, playerTournaments } = player
+
+    useEffect(() => {
         try {
-             await axios.get(url)
+             axios.get(url)
                 .then(res => {
-                    console.log(res)
                     setIsLoading(true)
-                    setPlayer(res.data[0])
+                    setAllTournaments(res.data[0])
+                    setPlayer(res.data[1][0])
                     setIsLoading(false)
                 })
-        } catch (e) {
+         } catch (e) {
             console.log(e)
         }
     }, [])
+
+
+    const selectTournamentsList = allTournaments.filter(tournament => {
+        return tournament.placements?.find(placement => placement.playerName === playerName)
+    })
+
+    console.log(selectTournamentsList)
+
+    // (estimated)
+    // unclear data tournaments (finished before hero)
+    // total buy-in
+    // total rake
+    // total winnings
+    // total profit
+    // average ROI
+    // average stake
+    // average profit
+
+     // (real)
+    // clear data tournaments (known final position)
+    // total buy-in
+    // total rake
+    // total winnings
+    // total profit
+    // average ROI
+    // average stake
+    // average profit
+
+    // playing dates
+    // playing times
 
     return (
         <div>
         {isLoading && <div>Loading user...</div>}
             {!isLoading && 
-            <>
-                <h2>{player?.playerName}</h2>
-                <hr />
-            </>
+                <>
+                    <h2>{playerName} ({playerCountry})</h2>
+                    <p>(Tournaments played: {playerIsHero ? allTournaments.length : playerTournaments.length})</p>
+                    <hr />
+                </>
             }
         </div>
     )
