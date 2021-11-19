4b3c6733ddf70b61685a7fc6c376ef34ded4d026
DominikFischer86 <dfischer@compeon.de>
2021-11-19 09:43:24 +0100

    Update spinner

--- frontend/src/pages/HomePage/__tests__/HomePage.test.js
+++ frontend/src/pages/HomePage/__tests__/HomePage.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from "@testing-library/react"
-import HomePage from '../homePage'
+import HomePage from '../HomePage'
 
 describe("HomePage", () => {
   it("renders", () => {
