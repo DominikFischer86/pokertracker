4e9187f72f8cb5109466443edf84c0272cf54bdb
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2021-11-25 01:35:20 +0100

    Finalize itm calculations

--- frontend/src/pages/PlayerPage/components/PlayerITMTab/helper.js
+++ frontend/src/pages/PlayerPage/components/PlayerITMTab/helper.js
@@ -1,8 +1,4 @@
-export const itmCalc = (tournaments, maxPlayers) => {
-    const sngOnly = tournaments.filter(tournament => {
-        return tournament.playerAmount <= maxPlayers
-    })
-
+export const itmCalc = (sngOnly, maxPlayers, bubble) => {
     const sngList = sngOnly.reduce((acc, obj) => {
         const key = obj["finalPosition"]
         if (!acc[key]) {
@@ -14,8 +10,25 @@ export const itmCalc = (tournaments, maxPlayers) => {
 
     const itmDistribution = []
     for(let i = 1; i <= maxPlayers; i++){
-        if (!sngList[i]) itmDistribution.push({placement: i, [i]: 0})
-        if (sngList[i]) itmDistribution.push({ placement: i, [i]: sngList[i]?.length})
+        const totalTournaments = sngOnly.length
+        const amountPerPlace = sngList[i]?.length
+
+        if (!sngList[i]) itmDistribution.push({
+            placement: i, 
+            [i]: 0,
+            totalCash: 0,
+            finishedTotal: amountPerPlace || 0,
+            finishedPercent: parseFloat(([amountPerPlace]/totalTournaments*100).toFixed(2)),
+            isBubble: bubble === i
+        })
+        if (sngList[i]) itmDistribution.push({ 
+            placement: i, 
+            [i]: amountPerPlace,
+            totalCash: parseFloat(sngList[i].reduce((acc, currentVal) => acc + currentVal.playerPrizeMoney,0).toFixed(2)),
+            finishedTotal: amountPerPlace || 0,
+            finishedPercent: parseFloat(([amountPerPlace]/totalTournaments*100).toFixed(2)),
+            isBubble: bubble === i
+        })
     }
 
     return itmDistribution
