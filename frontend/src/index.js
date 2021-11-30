532e5761158bbdea7f5e5535c5a23acff27e3863
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2021-11-30 23:21:57 +0100

    Add MetaContext

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,19 @@
 import "./index.scss"
-import React from "react"
+import React, { createContext } from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
 import { BrowserRouter } from "react-router-dom"
 
+const meta = {
+  heroName: "KeinKönich"
+}
+export const MetaContext = createContext(meta)
+
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <MetaContext.Provider value={meta}>
+      <App />
+    </MetaContext.Provider>
   </BrowserRouter>,
   document.getElementById("root")
 );
