6885fa4fe94eaf086549fabb75670359bc0f29cc
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2021-12-02 01:25:09 +0100

    Remove old components

--- frontend/src/pages/RakebackManager/components/RakebackManagerTable.js
+++ frontend/src/pages/RakebackManager/components/RakebackManagerTable.js
@@ -2,8 +2,6 @@ import React from "react"
 import { oneOfType, object, array, func } from "prop-types"
 import { FaTrashAlt } from "react-icons/fa"
 
-import { formatMonth, formatDay } from "../../ImportPage/helpers"
-
 const RakebackManagerTable = ({data, onDelete}) => {
     if (!data) return null
     return (
@@ -21,14 +19,12 @@ const RakebackManagerTable = ({data, onDelete}) => {
                 </thead>
                 <tbody>
                     {data.map(entry => {
-                        const transformDate = new Date(entry.redeemDate)
-                        const redeemDate = `${transformDate.getFullYear(transformDate)}/${formatMonth(transformDate)}/${formatDay(transformDate)}`
-
+                        
                         return (
                         <tr key={entry.rakebackId}>
                            <td>{entry.rakebackId}</td>
                            <td>{entry.heroName}</td>
-                           <td>{redeemDate}</td>
+                           <td>{entry.redeemDate}</td>
                            <td>{entry.rakebackType}</td>
                            <td>{entry.rakebackValue} USD</td>
                            <td>
