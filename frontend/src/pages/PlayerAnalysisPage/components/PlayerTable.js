8b4550bc43463ad291826e8812f9cc192ef2fa52
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2021-12-19 04:16:47 +0100

    Add pagination to player analysis

--- frontend/src/pages/PlayerAnalysisPage/components/PlayerTable.js
+++ frontend/src/pages/PlayerAnalysisPage/components/PlayerTable.js
@@ -1,53 +1,72 @@
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
+
 import { FaTrashAlt } from "react-icons/fa"
+
 import Spinner from "../../../components/Spinner/Spinner"
+import { Pagination } from "../../../hooks/Pagination"
 
 const PlayerTable = ({isLoading, players, onDelete}) => {
+    const [currentPage, setCurrentPage] = useState(1)
+    const [entriesPerPage] = useState(10)
 
-    const filteredPlayers = players.filter(player => player.playerTournaments.length > 4)
- 
+    const filteredPlayers = players.filter(player => player.playerTournaments.length > 9).sort((a, b) => b.playerTournaments.length - a.playerTournaments.length)
+
+    // Get current entries
+    const indexOfLastEntry = currentPage * entriesPerPage
+    const indexOfFirstEntry = indexOfLastEntry - entriesPerPage
+    const currentEntries = filteredPlayers.slice(indexOfFirstEntry, indexOfLastEntry)
+
+    const paginate = pageNumber => setCurrentPage(pageNumber)
+    
     return (
+        <>
         <div className="results_table">
-            {players?.length < 1 && <p>No players found</p>}
+            {filteredPlayers?.length < 1 && <p>No players found</p>}
             {isLoading && <Spinner />}
-            {!isLoading && players?.length > 0 &&
-                <table>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Name</th>
-                            <th>Country</th>
-                            <th>Played tournaments</th>
-                            <th>Actions</th>
+            {!isLoading && filteredPlayers?.length > 0 &&
+            <table>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Country</th>
+                        <th>Played tournaments</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>                     
+                    {currentEntries.map((player, i) => {
+                        return (
+                        <tr key={i} style={
+                            player.playerIsHero 
+                                ? {color: "rgb(0,0,150)", fontWeight: "500"} 
+                                : null                                            
+                            }>
+                            <td>#{i+1}</td>
+                            <td><a href={`/player/${player.playerId}`}>{player.playerName}</a></td>
+                            <td>{player.playerCountry}</td>
+                            <td>{player.playerTournaments.length}</td>
+                            <td>
+                                <FaTrashAlt 
+                                    onClick={() => onDelete(player.playerId)}
+                                    className="trash-icon"
+                                    style={{color: "red"}}
+                                />
+                            </td>                              
                         </tr>
-                    </thead>
-                    <tbody>                     
-                        {filteredPlayers.sort((a, b) => b.playerTournaments.length - a.playerTournaments.length).map((player, i) => {
-                            return (
-                            <tr key={i} style={
-                                player.playerIsHero 
-                                    ? {color: "rgb(0,0,150)", fontWeight: "500"} 
-                                    : null                                            
-                                }>
-                                <td>#{i+1}</td>
-                                <td><a href={`/player/${player.playerId}`}>{player.playerName}</a></td>
-                                <td>{player.playerCountry}</td>
-                                <td>{player.playerTournaments.length}</td>
-                                <td>
-                                    <FaTrashAlt 
-                                        onClick={() => onDelete(player.playerId)}
-                                        className="trash-icon"
-                                        style={{color: "red"}}
-                                    />
-                                </td>                              
-                            </tr>
-                            )
-                        })}
-                    </tbody>
-                </table>
-                }
+                        )
+                    })}
+                </tbody>
+            </table>
+          }
         </div>
+        <Pagination 
+            entriesPerPage={entriesPerPage} 
+            totalEntries={filteredPlayers.length}
+            paginate={paginate}
+        />
+    </>
     )
 }
 
