1cc5d243ae2727dc894b9c04ea04c5c69cc00172
DominikFischer86 <dfischer@compeon.de>
2021-12-23 16:54:50 +0100

    Fix and return hand history conversion

--- frontend/src/pages/ImportPage/handFileConverter.js
+++ frontend/src/pages/ImportPage/handFileConverter.js
@@ -1,7 +1,6 @@
 export const handFileConverter = (file, hero) => {
 
     const tournamentId = file.split("Tournament #")[1].split(", ")[0]
-    let finalBounty = 0
     const handsFromFileArray = file.split("PokerStars ").slice(1)
 
     const singleHand = handsFromFileArray.map(hand => {
@@ -17,18 +16,18 @@ export const handFileConverter = (file, hero) => {
         const handTime = handSplit[0].split(" - ")[2].split(" ")[1] + " " + handSplit[0].split(" - ")[2].split(" ")[2]
 
         // determine blinds and antes
-        const smallBlindAmount = changedHand.split("posts small blind ")[1]?.split("\r")[0]
-        const isSmallBlindPlayerPosition = changedHand.split(": posts small blind ")[0].split("\r\n").length
-        let isSmallBlindPlayer = changedHand.split(": posts small blind ")[0].split("\r\n")[isSmallBlindPlayerPosition-1]
+        const smallBlindAmount = changedHand.split("posts small blind ")[1]?.split("\n")[0]
+        const isSmallBlindPlayerPosition = changedHand.split(": posts small blind ")[0].split("\n").length
+        let isSmallBlindPlayer = changedHand.split(": posts small blind ")[0].split("\n")[isSmallBlindPlayerPosition-1]
         if (isSmallBlindPlayer.includes(hero)) isSmallBlindPlayer = hero
-        
-        const bigBlindAmount = changedHand.split("posts big blind ")[1]?.split("\r")[0]
-        const isBigBlindPlayerPosition = changedHand.split(": posts big blind ")[0].split("\r\n").length
-        let isBigBlindPlayer = changedHand.split(": posts big blind ")[0].split("\r\n")[isBigBlindPlayerPosition-1]
+
+        const bigBlindAmount = changedHand.split("posts big blind ")[1]?.split("\n")[0]
+        const isBigBlindPlayerPosition = changedHand.split(": posts big blind ")[0].split("\n").length
+        let isBigBlindPlayer = changedHand.split(": posts big blind ")[0].split("\n")[isBigBlindPlayerPosition-1]
         if (isBigBlindPlayer.includes(hero)) isBigBlindPlayer = hero
 
-        const anteAmount = changedHand.split("posts the ante ")[1]?.split("\r")[0]
-        
+        const anteAmount = changedHand.split("posts the ante ")[1]?.split("\n")[0]
+
         // determine hand meta
         const handMetaMap = {
             tournamentId,
@@ -51,7 +50,7 @@ export const handFileConverter = (file, hero) => {
             }
         }
 
-        seats.map(seat => {            
+        seats.map(seat => {
             const seatId = seat.split(":")[0].replace(" ", "_").toLowerCase()
             const isValidSeat = seat.split(":")[0]?.includes("Seat")
             const isOutOfHand = seat.split(":")[1]?.includes("out of hand")
@@ -61,7 +60,7 @@ export const handFileConverter = (file, hero) => {
             const seatedPlayerStack = seat?.split(" (")[1]?.split(" in")[0]
             const seatedPlayerBounty = isBounty && !isOutOfHand ? seat?.split(", $")[1]?.split(" bounty")[0] : 0
             const isSittingOut = seat.includes("is sitting out")
- 
+
             seatsMap = {
                 ...seatsMap,
                 [seatId]: {
@@ -79,17 +78,17 @@ export const handFileConverter = (file, hero) => {
         })
 
         // Determine preflop action
-        const preflopActionsStartPosition = changedHand.split("\n*** HOLE CARDS ***")[0].split("\r\n").length
-        const preflopActionEndPosition = changedHand.split("\n*** FLOP ***")[0].split("\r\n").length
-        const preflopActionEndPositionSummary = changedHand.split("\n*** SUMMARY ***")[0].split("\r\n").length
-        const isTruePreflopActionEndPosition = preflopActionEndPosition !== handLength 
-            ? preflopActionEndPosition 
-            : preflopActionEndPositionSummary 
+        const preflopActionsStartPosition = changedHand.split("\n*** HOLE CARDS ***")[0].split("\n").length
+        const preflopActionEndPosition = changedHand.split("\n*** FLOP ***")[0].split("\n").length
+        const preflopActionEndPositionSummary = changedHand.split("\n*** SUMMARY ***")[0].split("\n").length
+        const isTruePreflopActionEndPosition = preflopActionEndPosition !== handLength
+            ? preflopActionEndPosition
+            : preflopActionEndPositionSummary
 
         const preflopActions = changedHand.split("\n").splice(
-            preflopActionsStartPosition + 1, 
+            preflopActionsStartPosition + 1,
             isTruePreflopActionEndPosition - preflopActionsStartPosition - 1)
-        
+
         let preflopStory = null
         let flopStory = null
         let turnStory = null
@@ -106,12 +105,12 @@ export const handFileConverter = (file, hero) => {
 
             const activePlayerSeat = Object.values(seatsMap).find(seat => seat.playerName === activePlayerName)?.playerSeat
 
-            const storyBlackList = 
+            const storyBlackList =
                 action.split(" ")[0] === "Uncalled" ||
                 action.split(": ")[1]?.split(" ")[0] === "to" ||
                 action.split(" ")[1]?.split(" show")[0] === "doesn't" ||
                 action.split(": ")[1]?.split(" ")[0] === undefined
-                
+
             if (!storyBlackList){
                 const isRaise = action.split(": ")[1]?.split(" ")[0] === "raises"
                 const isCall = action.split(": ")[1]?.split(" ")[0] === "calls"
@@ -120,17 +119,17 @@ export const handFileConverter = (file, hero) => {
                     activePlayerName,
                     activePlayerSeat,
                     action.split(": ")[1].split(" ")[0],
-                    isRaise 
-                        ? parseFloat(action.split("to ")[1]) 
-                        : (isCall 
+                    isRaise
+                        ? parseFloat(action.split("to ")[1])
+                        : (isCall
                             ? parseFloat(action.split("calls ")[1])
                             : 0
                         ),
                     (isRaise || isCall ) && action.includes("all-in") ? "All-In" : ""
                 ])
             }
-       
-            
+
+
             if (!activePlayerSeat) return
             preflopStory = {
                 ...preflopStory,
@@ -142,18 +141,18 @@ export const handFileConverter = (file, hero) => {
         story = []
 
         // determine flop action
-        const flopActionStartPosition = changedHand.split("\n*** FLOP ***")[0].split("\r\n").length
-        const flopActionEndPosition = changedHand.split("\n*** TURN ***")[0].split("\r\n").length
-        const flopActionEndPositionSummary = changedHand.split("\n*** SUMMARY ***")[0].split("\r\n").length
-        const isTrueFlopActionEndPosition = flopActionEndPosition !== handLength 
-            ? flopActionEndPosition 
+        const flopActionStartPosition = changedHand.split("\n*** FLOP ***")[0].split("\n").length
+        const flopActionEndPosition = changedHand.split("\n*** TURN ***")[0].split("\n").length
+        const flopActionEndPositionSummary = changedHand.split("\n*** SUMMARY ***")[0].split("\n").length
+        const isTrueFlopActionEndPosition = flopActionEndPosition !== handLength
+            ? flopActionEndPosition
             : flopActionEndPositionSummary
 
-        
+
         const flopActions = changedHand.split("\n").splice(
-            flopActionStartPosition, 
+            flopActionStartPosition,
             isTrueFlopActionEndPosition - flopActionStartPosition)
-        
+
         if (flopActions.length > 0) {
             flopActions.map((action,index) => {
                 if (index < 1 ) board = action.split("[")[1]?.split("]")[0]
@@ -171,13 +170,13 @@ export const handFileConverter = (file, hero) => {
                         activePlayerName,
                         activePlayerSeat,
                         action.split(": ")[1].split(" ")[0],
-                        isRaise 
-                            ? parseFloat(action.split("to ")[1]) 
-                            : (isCall 
+                        isRaise
+                            ? parseFloat(action.split("to ")[1])
+                            : (isCall
                                 ? parseFloat(action.split("calls ")[1])
-                                : (isBet 
+                                : (isBet
                                     ? parseFloat(action.split("bets ")[1])
-                                    : 0    
+                                    : 0
                                 )
                             ),
                         (isRaise || isCall ) && action.includes("all-in") ? "All-In" : ""
@@ -196,27 +195,27 @@ export const handFileConverter = (file, hero) => {
         story = []
 
         // determine turn action
-        const turnActionStartPosition = changedHand.split("\n*** TURN ***")[0].split("\r\n").length
-        const turnActionEndPosition = changedHand.split("\n*** RIVER ***")[0].split("\r\n").length
-        const turnActionEndPositionSummary = changedHand.split("\n*** SUMMARY ***")[0].split("\r\n").length
-        const isTrueTurnActionEndPosition = turnActionEndPosition !== handLength 
-            ? turnActionEndPosition 
+        const turnActionStartPosition = changedHand.split("\n*** TURN ***")[0].split("\n").length
+        const turnActionEndPosition = changedHand.split("\n*** RIVER ***")[0].split("\n").length
+        const turnActionEndPositionSummary = changedHand.split("\n*** SUMMARY ***")[0].split("\n").length
+        const isTrueTurnActionEndPosition = turnActionEndPosition !== handLength
+            ? turnActionEndPosition
             : turnActionEndPositionSummary
 
-        
+
         const turnActions = changedHand.split("\n").splice(
-            turnActionStartPosition, 
+            turnActionStartPosition,
             isTrueTurnActionEndPosition - turnActionStartPosition)
-        
+
         if (turnActions.length > 0) {
             turnActions.map((action,index) => {
                 const leftBracket = /\[/gi
                 const rightBracket = /\]/gi
-                if (index < 1 ) board = action.split("*** ")[2].split("\r")[0].replace(leftBracket, "").replace(rightBracket, "")
+                if (index < 1 ) board = action.split("*** ")[2].split("\n")[0].replace(leftBracket, "").replace(rightBracket, "")
                 let activePlayerName = action.split(":")[0]
                 const activePlayerSeat = Object.values(seatsMap).find(seat => seat.playerName === activePlayerName)?.playerSeat
 
-                const storyBlackList = 
+                const storyBlackList =
                     action.split(" ")[1]?.split(" show")[0] === "doesn't" ||
                     action.split(": ")[1]?.split(" ")[0] === undefined
 
@@ -229,13 +228,13 @@ export const handFileConverter = (file, hero) => {
                         activePlayerName,
                         activePlayerSeat,
                         action.split(": ")[1].split(" ")[0],
-                        isRaise 
-                            ? parseFloat(action.split("to ")[1]) 
-                            : (isCall 
+                        isRaise
+                            ? parseFloat(action.split("to ")[1])
+                            : (isCall
                                 ? parseFloat(action.split("calls ")[1])
-                                : (isBet 
+                                : (isBet
                                     ? parseFloat(action.split("bets ")[1])
-                                    : 0    
+                                    : 0
                                 )
                             ),
                         (isRaise || isCall ) && action.includes("all-in") ? "All-In" : ""
@@ -254,27 +253,27 @@ export const handFileConverter = (file, hero) => {
         story = []
 
         // determine river action
-        const riverActionStartPosition = changedHand.split("\n*** RIVER ***")[0].split("\r\n").length
-        const riverActionEndPosition = changedHand.split("\n*** SUMMARY ***")[0].split("\r\n").length
-        const riverActionEndPositionSummary = changedHand.split("\n*** SUMMARY ***")[0].split("\r\n").length
-        const isTrueRiverActionEndPosition = riverActionEndPosition !== handLength 
-            ? riverActionEndPosition 
+        const riverActionStartPosition = changedHand.split("\n*** RIVER ***")[0].split("\n").length
+        const riverActionEndPosition = changedHand.split("\n*** SUMMARY ***")[0].split("\n").length
+        const riverActionEndPositionSummary = changedHand.split("\n*** SUMMARY ***")[0].split("\n").length
+        const isTrueRiverActionEndPosition = riverActionEndPosition !== handLength
+            ? riverActionEndPosition
             : riverActionEndPositionSummary
 
-        
+
         const riverActions = changedHand.split("\n").splice(
-            riverActionStartPosition, 
+            riverActionStartPosition,
             isTrueRiverActionEndPosition - riverActionStartPosition)
-        
+
         if (riverActions.length > 0) {
             riverActions.map((action,index) => {
                 const leftBracket = /\[/gi
                 const rightBracket = /\]/gi
-                if (index < 1 ) board = action.split("*** ")[2].split("\r")[0].replace(leftBracket, "").replace(rightBracket, "")
+                if (index < 1 ) board = action.split("*** ")[2].split("\n")[0].replace(leftBracket, "").replace(rightBracket, "")
                 let activePlayerName = action.split(":")[0]
                 const activePlayerSeat = Object.values(seatsMap).find(seat => seat.playerName === activePlayerName)?.playerSeat
 
-                const storyBlackList = 
+                const storyBlackList =
                     action.split(" ")[1]?.split(" show")[0] === "doesn't" ||
                     action.split(": ")[1]?.split(" ")[0] === "shows" ||
                     action.split(": ")[1]?.split(" ")[0] === "mucks" ||
@@ -289,13 +288,13 @@ export const handFileConverter = (file, hero) => {
                         activePlayerName,
                         activePlayerSeat,
                         action.split(": ")[1].split(" ")[0],
-                        isRaise 
-                            ? parseFloat(action.split("to ")[1]) 
-                            : (isCall 
+                        isRaise
+                            ? parseFloat(action.split("to ")[1])
+                            : (isCall
                                 ? parseFloat(action.split("calls ")[1])
-                                : (isBet 
+                                : (isBet
                                     ? parseFloat(action.split("bets ")[1])
-                                    : 0    
+                                    : 0
                                 )
                             ),
                         (isRaise || isCall ) && action.includes("all-in") ? "All-In" : ""
@@ -314,7 +313,7 @@ export const handFileConverter = (file, hero) => {
         story = []
 
         // determine hand summary
-        const summaryStartPosition = changedHand.split("\n*** SUMMARY ***")[0].split("\r\n").length
+        const summaryStartPosition = changedHand.split("\n*** SUMMARY ***")[0].split("\n").length
         const summaryActions = board
             ? changedHand.split("\n").splice(summaryStartPosition + 3, 9)
             : changedHand.split("\n").splice(summaryStartPosition + 2, 9)
@@ -322,7 +321,7 @@ export const handFileConverter = (file, hero) => {
         summaryActions.map(action => {
             const isButton = action.includes("button")
             const isSmallBlind = action.includes("small blind")
-            if (isSmallBlind) action = action.replace("small blind", "small_blind")  
+            if (isSmallBlind) action = action.replace("small blind", "small_blind")
 
             const isBigBlind = action.includes("big blind")
             if (isBigBlind) action = action.replace("big blind", "big_blind")
@@ -333,7 +332,7 @@ export const handFileConverter = (file, hero) => {
             const isMucked = action.includes("mucked")
 
             const isDifferentFormat = isButton || isSmallBlind || isBigBlind
-            
+
             //Find Username with spaces:
             const playerNameWithSpacesFromSeats = Object.values(seatsMap).find(seat => seat.playerName?.includes(" "))?.playerName
             const spacesInName = playerNameWithSpacesFromSeats ? playerNameWithSpacesFromSeats.split(" ").length - 1 : 0
@@ -341,17 +340,17 @@ export const handFileConverter = (file, hero) => {
             if (spacesInName > 0 && action.includes(playerNameWithSpacesFromSeats)) activePlayerName = playerNameWithSpacesFromSeats
             if (activePlayerName && isDifferentFormat) activePlayerName = action.split(": ")[1]?.split(" (")[0]
             const activePlayerSeat = Object.values(seatsMap).find(seat => seat.playerName === activePlayerName)?.playerSeat
-            const storyBlackList = 
-                action.includes("folded") || 
+            const storyBlackList =
+                action.includes("folded") ||
                 !action.includes("Seat")
-            
+
             if (isDifferentFormat) action = action.replace("(", "").replace(")", "")
 
             if (!storyBlackList) {
                 if (isCollected) story.push([
                     activePlayerName,
                     activePlayerSeat,
-                    isDifferentFormat 
+                    isDifferentFormat
                         ? action.split(" (")[0]?.split(activePlayerName)[1]?.split(" ")[2]
                         : action.split(" (")[0]?.split(activePlayerName)[1],
                     parseFloat(action.split("(")[1]?.split(")")[0])
@@ -360,13 +359,13 @@ export const handFileConverter = (file, hero) => {
                     activePlayerName,
                     activePlayerSeat,
                     // action
-                    action.split(" and ")[1].split(" (")[0],
+                    action.split(" and ")[1]?.split(" (")[0],
                     // pot
-                    parseFloat(action.split("(")[1].split(")")[0]),
+                    parseFloat(action.split("(")[1]?.split(")")[0]),
                     // hole cards
-                    action.split("[")[1].split("]")[0],
+                    action.split("[")[1]?.split("]")[0],
                     // hand
-                    action.split(") with ")[1].split("\r")[0]
+                    action.split(") with ")[1]?.split("\n")[0]
                 ])
                 if (isShowAndLost) story.push([
                     activePlayerName,
@@ -378,7 +377,7 @@ export const handFileConverter = (file, hero) => {
                     // hole cards
                     action.split("[")[1].split("]")[0],
                     // hand
-                    action.split("with ")[1].split("\r")[0]
+                    action.split("with ")[1].split("\n")[0]
                 ])
                 if (isMucked) story.push([
                     activePlayerName,
@@ -390,7 +389,7 @@ export const handFileConverter = (file, hero) => {
                     // hole cards
                     action.split("[")[1].split("]")[0]
                 ])
-                
+
             }
 
             if (!activePlayerSeat) return
@@ -414,4 +413,6 @@ export const handFileConverter = (file, hero) => {
 
         return handMap
     })
+
+    return singleHand
 }
