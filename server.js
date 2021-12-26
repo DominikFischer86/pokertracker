957c2895ef41eec87c4e630490fdc16fbdfb2293
DominikFischer86 <dfischer@compeon.de>
2021-12-26 23:39:45 +0100

    Add hand history routes to server

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.TOURNAMENT_DB_LOCAL)
 require("./backend/routes/player.routes")(app)
 require("./backend/routes/tournament.routes")(app)
 require("./backend/routes/rakeback.routes")(app)
-
+require("./backend/routes/handHistory.routes")(app)
 
 app.listen(PORT, function(){
     console.log("Express is running!")
