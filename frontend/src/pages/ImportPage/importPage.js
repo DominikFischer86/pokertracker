1bb5985a885a1c1c1346d581769800745d3e9746
DominikFischer86 <dfischer@compeon.de>
2021-12-27 23:39:43 +0100

    Post split stories to db

--- frontend/src/pages/ImportPage/importPage.js
+++ frontend/src/pages/ImportPage/importPage.js
@@ -33,17 +33,33 @@ const ImportPage = () => {
     const [modalContent, setModalContent] = useState({successMessageList: [], warningMessageList: [], errorMessageList:[]})
 
     const tabs = ["Tournaments", "Hand Histories"]
+    console.log(hands)
 
     const tournamentAndHandsGetUrl = "http://localhost:3001/hand-histories-and-tournaments"
     const tournamentPostUrl = "http://localhost:3001/tournaments/add"
-    const handHistoryPostUrl = "http://localhost:3001/hand-histories/add"
+    const handHistoryPostMetaUrl = "http://localhost:3001/hand-histories/add/meta"
+    // const handHistoryPostSeatsUrl = "http://localhost:3001/hand-histories/add/seats"
+    const handHistoryPostSeat1Url = "http://localhost:3001/hand-histories/add/seat_1"
+    const handHistoryPostSeat2Url = "http://localhost:3001/hand-histories/add/seat_2"
+    const handHistoryPostSeat3Url = "http://localhost:3001/hand-histories/add/seat_3"
+    const handHistoryPostSeat4Url = "http://localhost:3001/hand-histories/add/seat_4"
+    const handHistoryPostSeat5Url = "http://localhost:3001/hand-histories/add/seat_5"
+    const handHistoryPostSeat6Url = "http://localhost:3001/hand-histories/add/seat_6"
+    const handHistoryPostSeat7Url = "http://localhost:3001/hand-histories/add/seat_7"
+    const handHistoryPostSeat8Url = "http://localhost:3001/hand-histories/add/seat_8"
+    const handHistoryPostSeat9Url = "http://localhost:3001/hand-histories/add/seat_9"
+    const handHistoryPostPreflopUrl = "http://localhost:3001/hand-histories/add/preflop"
+    const handHistoryPostFlopUrl = "http://localhost:3001/hand-histories/add/flop"
+    const handHistoryPostTurnUrl = "http://localhost:3001/hand-histories/add/turn"
+    const handHistoryPostRiverUrl = "http://localhost:3001/hand-histories/add/river"
+    const handHistoryPostSummaryUrl = "http://localhost:3001/hand-histories/add/summary"
 
     useEffect(() => {
         try {
             axios.get(tournamentAndHandsGetUrl)
                 .then(res => {
-                    setHands(res.data[0])
-                    setTournaments(res.data[1])
+                    setTournaments(res.data[0])
+                    setHands(res.data[1])
                 })
         } catch (e) {
             console.log(e)
@@ -117,35 +133,102 @@ const ImportPage = () => {
         setSkipFilePlacements(false)
     }
 
-    const submitHandData = () => {
+    const submitHandData = async () => {
         let successMessageList = []
         let warningMessageList = []
 
-        handMap.forEach(handHistory => {
+        for (let handHistory of handMap){
+            let metaMap = []
+            // let seatsMap = []
+            let seat_1Map = []
+            let seat_2Map = []
+            let seat_3Map = []
+            let seat_4Map = []
+            let seat_5Map = []
+            let seat_6Map = []
+            let seat_7Map = []
+            let seat_8Map = []
+            let seat_9Map = []
+            let preflopMap = []
+            let flopMap = []
+            let turnMap = []
+            let riverMap = []
+            let summaryMap = []
             const handHistoryLength = handHistory.length
             const lastHand = handHistory[handHistoryLength-1]
-            const handMeta = handHistory[0]["1_meta"]
             const finalBounty = Object.values(lastHand["2_seats"])?.find(seat => seat.playerName === heroName).playerBounty
-            const tournamentId = handHistory[0].tournamentId
+            const tournamentId = handHistory.find(hand => hand["1_meta"].tournamentId)["1_meta"].tournamentId
 
-            const tournamentExists = hands.some(
-                element => element.handHistory[0].tournamentId === tournamentId)
+            const tournamentExists = hands.some(element => element.meta[0].tournamentId === tournamentId)
 
             if (finalBounty > 0 && tournamentExists) updateBountyInTournament(finalBounty, tournamentId)
 
             if (tournamentExists) {
                 console.log(`%c Tournament with these hands #${tournamentId} already exists.`, "color: orange")
-                return warningMessageList.push(`Tournament with these hands #${tournamentId} already exists.`)
+                warningMessageList.push(`Tournament with these hands #${tournamentId} already exists.`)
             }
 
-            axios.post(handHistoryPostUrl, handHistory)
-            successMessageList.push(` Added Hand History for tournament: #${tournamentId}`)
-            console.log(`%c Added Hand History for tournament: #${tournamentId}`, "color: green")
+            for (let hand of handHistory){
+                const meta = Object.keys(hand["1_meta"]).length > 0 ? hand["1_meta"] : null
+                if (meta !== null) metaMap.push(meta)
+                // const seats = Object.keys(hand["2_seats"]).length > 0 ? hand["2_seats"] : null
+                // if (seats !== null) seatsMap.push(seats)
 
-        })
+                const seat_1 = Object.keys(hand["2_seats"]["seat_1"])?.length > 0 ? hand["2_seats"]["seat_1"] : null
+                if (seat_1 !== null) seat_1Map.push(seat_1)
+                const seat_2 = Object.keys(hand["2_seats"]["seat_2"])?.length > 0 ? hand["2_seats"]["seat_2"] : null
+                if (seat_2 !== null) seat_2Map.push(seat_2)
+                const seat_3 = Object.keys(hand["2_seats"]["seat_3"])?.length > 0 ? hand["2_seats"]["seat_3"] : null
+                if (seat_3 !== null) seat_3Map.push(seat_3)
+                const seat_4 = Object.keys(hand["2_seats"]["seat_4"])?.length > 0 ? hand["2_seats"]["seat_4"] : null
+                if (seat_4 !== null) seat_4Map.push(seat_4)
+                const seat_5 = Object.keys(hand["2_seats"]["seat_5"])?.length > 0 ? hand["2_seats"]["seat_5"] : null
+                if (seat_5 !== null) seat_5Map.push(seat_5)
+                const seat_6 = Object.keys(hand["2_seats"]["seat_6"])?.length > 0 ? hand["2_seats"]["seat_6"] : null
+                if (seat_6 !== null) seat_6Map.push(seat_6)
+                const seat_7 = Object.keys(hand["2_seats"]["seat_7"])?.length > 0 ? hand["2_seats"]["seat_7"] : null
+                if (seat_7 !== null) seat_7Map.push(seat_7)
+                const seat_8 = Object.keys(hand["2_seats"]["seat_8"])?.length > 0 ? hand["2_seats"]["seat_8"] : null
+                if (seat_8 !== null) seat_8Map.push(seat_8)
+                const seat_9 = Object.keys(hand["2_seats"]["seat_9"])?.length > 0 ? hand["2_seats"]["seat_9"] : null
+                if (seat_9 !== null) seat_9Map.push(seat_9)
+
+                const preflop = Object.keys(hand["3_preflop"]).length > 0 ? hand["3_preflop"] : null
+                if (preflop !== null) preflopMap.push(preflop)
+                const flop = Object.keys(hand["4_flop"]).length > 0 ? hand["4_flop"] : null
+                if (flop !== null) flopMap.push(flop)
+                const turn = Object.keys(hand["5_turn"]).length > 0 ? hand["5_turn"] : null
+                if (turn !== null) turnMap.push(turn)
+                const river = Object.keys(hand["6_river"]).length > 0 ? hand["6_river"] : null
+                if (river !== null) riverMap.push(river)
+                const summary = Object.keys(hand["7_summary"]).length > 0 ? hand["7_summary"] : null
+                if (summary !== null) summaryMap.push(summary)
+            }
+
+            if (!tournamentExists){
+                await axios.post(handHistoryPostMetaUrl, metaMap)
+                .then(() => axios.post(handHistoryPostSeat1Url, seat_1Map))
+                .then(() => axios.post(handHistoryPostSeat2Url, seat_2Map))
+                .then(() => axios.post(handHistoryPostSeat3Url, seat_3Map))
+                .then(() => axios.post(handHistoryPostSeat4Url, seat_4Map))
+                .then(() => axios.post(handHistoryPostSeat5Url, seat_5Map))
+                .then(() => axios.post(handHistoryPostSeat6Url, seat_6Map))
+                .then(() => axios.post(handHistoryPostSeat7Url, seat_7Map))
+                .then(() => axios.post(handHistoryPostSeat8Url, seat_8Map))
+                .then(() => axios.post(handHistoryPostSeat9Url, seat_9Map))
+                .then(() => axios.post(handHistoryPostPreflopUrl, preflopMap))
+                .then(() => axios.post(handHistoryPostFlopUrl, flopMap))
+                .then(() => axios.post(handHistoryPostTurnUrl, turnMap))
+                .then(() => axios.post(handHistoryPostRiverUrl, riverMap))
+                .then(() => axios.post(handHistoryPostSummaryUrl, summaryMap))
+
+                successMessageList.push(` Added Hand History for tournament: #${tournamentId}`)
+                console.log(`%c Added Hand History for tournament: #${tournamentId}`, "color: green")
+            }
+
+        }
 
         const messageLists = {successMessageList, warningMessageList}
-        console.log(messageLists)
         setModalContent(messageLists)
         openModal()
         setHandPreviewExpanded(false)
