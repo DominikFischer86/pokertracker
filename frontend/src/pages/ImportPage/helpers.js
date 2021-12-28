447c8e1a34b8782c859307f5e9bddc1bc95b879e
DominikFischer86 <dfischer@compeon.de>
2021-12-28 18:05:09 +0100

    Remove formatPosition

--- frontend/src/pages/ImportPage/helpers.js
+++ frontend/src/pages/ImportPage/helpers.js
@@ -63,16 +63,3 @@ export const formatSecond = timeStamp => {
     let second = timeStamp.getSeconds()
     return second < 10 ? '0' + second : "" + second
 }
-
-export const formatPosition = (isValidSeat, seatId, isBigBlind) => {
-    console.log(isValidSeat, seatId, isBigBlind)
-    const tenActivePlayers = ["UTG", "UTG+1", "UTG+2", "UTG+3", "LJ", "HJ", "CO", "BU", "SB", "BB"]
-    const nineActivePlayers = ["UTG", "UTG+1", "UTG+2", "LJ", "HJ", "CO", "BU", "SB", "BB"]
-    const eightActivePlayers = ["UTG", "UTG+1", "LJ", "HJ", "CO", "BU", "SB", "BB"]
-    const sevenActivePlayers = ["UTG", "LJ", "HJ", "CO", "BU", "SB", "BB"]
-    const sixActivePlayers = ["LJ", "HJ", "CO", "BU", "SB", "BB"]
-    const fiveActivePlayers = ["HJ", "CO", "BU", "SB", "BB"]
-    const fourActivePlayers = ["CO", "BU", "SB", "BB"]
-    const threeActivePlayers = ["BU", "SB", "BB"]
-    const twoActivePlayers = ["SB", "BB"]
-}
