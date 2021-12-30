40a096419071c26cdf527d8304f34c880c97af68
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2021-12-30 15:07:55 +0100

    Add Seat component to Replayer and loop through

--- frontend/src/pages/HandReplayerPage/components/Replayer.js
+++ frontend/src/pages/HandReplayerPage/components/Replayer.js
@@ -5,15 +5,16 @@ import { FaCoins } from "react-icons/fa"
 
 import Spinner from "../../../components/Spinner/Spinner"
 import { MetaContext } from "../../../index"
-import { formatPosition } from "../helper"
+
+import Seat from "./Seat"
 
 const Replayer = ({tournament, activeHand}) => {
   const { appName } = useContext(MetaContext)
   if (Object.keys(activeHand).length < 1) return <Spinner message="Select a hand" />
   const { smallBlind, bigBlind, level, date, time } = activeHand["1_meta"]
-  const { playerOutOfHand, playerSitOut, playerSeat, playerName, playerStack, playerBigBlind } = activeHand["2_seat_1"]
-  const position = formatPosition(!playerOutOfHand, playerSeat, playerBigBlind > 0)
-  console.log(tournament)
+  const playerPositions = ["2_seat_1", "3_seat_2", "4_seat_3", "5_seat_4", "6_seat_5", "7_seat_6", "8_seat_7", "9_seat_8", "10_seat_9"]
+
+  // console.log(tournament)
   console.log(activeHand)
 
   return (
@@ -27,11 +28,13 @@ const Replayer = ({tournament, activeHand}) => {
         </div>
         <div className="table">
           <p className="appTitle">{appName}</p>
-          <div className="seat">
-            <p>{playerSeat}</p>
-            <p>{playerName}</p>
-            <p>{playerStack}</p>
-          </div>
+            {playerPositions.map(position => {
+              const seat = "seat_" + position.split("seat_")[1]
+
+              return (
+                <Seat key={position} seat={seat} seatedPlayer={activeHand[position]} />
+              )
+            })}          
         </div>
       </div>
     </div>
