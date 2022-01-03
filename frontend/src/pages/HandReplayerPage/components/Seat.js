55d6208e95e4a591cc173315b7880c99f1fb67f5
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2022-01-04 00:01:42 +0100

    Complete HUD for stacks and stacksInBb

--- frontend/src/pages/HandReplayerPage/components/Seat.js
+++ frontend/src/pages/HandReplayerPage/components/Seat.js
@@ -5,7 +5,7 @@ import Stack from "./Stack"
 
 import "./styles/Seat.scss"
 
-const Seat = ({seat, seatedPlayer, toggleBlindUnits, toggleNames, heroName, bigBlind, initialPot, activePlayers}) => {
+const Seat = ({seat, seatedPlayer, toggleBlindUnits, toggleNames, heroName, bigBlind, initialPot, activePlayers, maxPlayers, bet, playerAction}) => {
     if (!seatedPlayer) {
         return (
             <div className={`seat seat_empty ${seat}`}>
@@ -16,27 +16,26 @@ const Seat = ({seat, seatedPlayer, toggleBlindUnits, toggleNames, heroName, bigB
         )
     }
     const { playerOutOfHand, playerSitOut, playerPosition, playerSeat, playerName, playerStack } = seatedPlayer
+    const smallBlind = bigBlind / 2
     const playerSmallBlind = seatedPlayer?.playerSmallBlind
     const playerBigBlind = seatedPlayer?.playerBigBlind
-    const playerAnte = seatedPlayer?.playerAnte
     const positionClass = (playerPosition.replace("+", "")).toLowerCase()
     const stackInBB = parseFloat(playerStack / bigBlind).toFixed(0)
     const newName = playerName === heroName ? "Hero" : "Villain " + seat.split("_")[1]
     const Mfactor = parseFloat(playerStack / initialPot).toFixed(2)
-    const effectiveM = (Mfactor * (activePlayers / 8)).toFixed(2)
-
-    let bet = 50000
+    const effectiveM = (Mfactor * (activePlayers / maxPlayers)).toFixed(2)
     
     if (playerSmallBlind > 0) bet = playerSmallBlind
     if (playerBigBlind > 0) bet = playerBigBlind
 
-    let betInBb = parseFloat(bet / bigBlind).toFixed(0)
+    // Use smallblind here to have 1 chip be worth a half bb for stack builds
+    let betInBb = parseFloat(bet / smallBlind).toFixed(1)
 
-    const buildStackArray = betAmountInBB => {        
+    const buildStackArray = betAmountInBB => {       
+        const roundedBetAmountInBB = Math.floor(betAmountInBB) 
         let outerArray = []
-        const betAmountInBBArray = betAmountInBB.toString().replace(/0/g, "1").split("").reverse()
+        const betAmountInBBArray = roundedBetAmountInBB.toString().replace(/0/g, "1").split("").reverse()
         betAmountInBBArray.forEach(item => item.replace("0", "1"))
-        console.log(betAmountInBBArray)
         betAmountInBBArray.forEach(item => {
             let innerArray = []
             for (let i = 1; i <= parseFloat(item); i++){
@@ -46,9 +45,6 @@ const Seat = ({seat, seatedPlayer, toggleBlindUnits, toggleNames, heroName, bigB
         })
         return outerArray
     }
-
-    // console.log(buildStackArray(betInBb))
-    const playerAction = null
 
     return (
         <div className={`seat ${playerSeat} ${positionClass}`}>
@@ -64,10 +60,10 @@ const Seat = ({seat, seatedPlayer, toggleBlindUnits, toggleNames, heroName, bigB
                 <p className="playerStack">{playerAction}</p>
             }
             {!playerOutOfHand && !playerSitOut && !playerAction && 
-                <p className="playerStack">{toggleBlindUnits ? `${stackInBB} | M: ${Mfactor}` : playerStack}</p>
+                <p title={`M = ${Mfactor} | effM = ${effectiveM}`} className="playerStack">{toggleBlindUnits ? `${stackInBB} BB` : playerStack}</p>
             }
             {bet > 0 &&
-                <div className="bet">
+                <div className={`bet stacks-${Math.floor(betInBb).toString().length}`}>
                     {buildStackArray(betInBb).map((item, index) => 
                         <div className="stackWrapper" key={index}  style={{ left: index * -28 + "px" }} >
                             <Stack                                 
@@ -75,8 +71,11 @@ const Seat = ({seat, seatedPlayer, toggleBlindUnits, toggleNames, heroName, bigB
                                 stackAmount={index+1}
                             />
                         </div>)
-                    }            
-                    <p>{!toggleBlindUnits ? bet: betInBb}</p>
+                    }
+                    <p>{
+                        // double halfed betInBb (which is in SB now) to have correct number values again 
+                        !toggleBlindUnits ? bet: (betInBb / 2).toFixed(1)}
+                    </p>
                 </div>
             }
         </div>
@@ -91,7 +90,10 @@ Seat.propTypes = {
     heroName: string,
     bigBlind: number,
     initialPot: number,
-    activePlayers: number
+    activePlayers: number,
+    maxPlayers: number,
+    bet: number,
+    playerAction: string
 }
 
 export default Seat
