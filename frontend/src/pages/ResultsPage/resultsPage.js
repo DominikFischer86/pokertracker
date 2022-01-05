9bef3bfb6bced625d3c246d2f392ac93b04b1d47
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2022-01-06 00:38:39 +0100

    Add filter to resultsPage

--- frontend/src/pages/ResultsPage/resultsPage.js
+++ frontend/src/pages/ResultsPage/resultsPage.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
 
+import { Switch } from "@react-md/form"
 import { TabsManager, Tabs, TabPanels, TabPanel } from "@react-md/tabs"
 
 import ResultsGraph from "./components/ResultsGraph/ResultsGraph"
 import ResultsFolder from "./components/ResultsFolder/ResultsFolder"
+import { Filters } from "../../components/Filters/Filters"
 
 import { createFolders } from "./helpers"
+
+import "./ResultsPage.scss"
 
 const ResultsPage = () => {
     const [tournaments, setTournaments] = useState([])
@@ -16,7 +20,10 @@ const ResultsPage = () => {
     const [dateFormattedTournaments, setDateFormattedTournaments] = useState({})
     const [isLoading, setIsLoading] = useState(true)
     const [refetch, setRefetch] = useState(0)
-
+    const [toggleFilter, setToggleFilter] = useState(false)
+    const [toggleHandFilter, setToggleHandFilter] = useState(false)
+    const [filteredTournaments, setFilteredTournaments] = useState(tournaments)
+    
     const tabs = ["Folder", "Graphs"]
 
     const getUrl = "http://localhost:3001/tournaments/dateSorted"
@@ -28,6 +35,7 @@ const ResultsPage = () => {
                 .then(res => {
                     setIsLoading(false)
                     setTournaments(res.data[0])
+                    setFilteredTournaments(res.data[0])
                     setRakebackData(res.data[1])
                     setHands(res.data[2])
                     createFolders(res.data[0], setSortedTournaments, setDateFormattedTournaments)
@@ -50,13 +58,51 @@ const ResultsPage = () => {
         setRefetch(refetch+1)
     }
 
+    const filterHands = () => {
+        if (!toggleHandFilter) {
+            setToggleHandFilter(!toggleHandFilter)
+            return createFolders(filteredTournaments, setSortedTournaments, setDateFormattedTournaments)           
+        }
+
+        const trackedHands = []
+        hands.forEach(hand => hand.meta.find(item => trackedHands.push(item.tournamentId)))
+        const tournamentsWithTrackedHands = filteredTournaments.filter(tournament => trackedHands.find(id => tournament.tournamentId === id))
+        createFolders(tournamentsWithTrackedHands, setSortedTournaments, setDateFormattedTournaments)
+        setToggleHandFilter(!toggleHandFilter)
+    }
+
     if (tournaments?.length == 0) return <div>No tournaments in database</div>
 
     return (
         <div>
-            <div className="ResultsTitleContainer">
+            <div className="ResultsTitleContainer subheader">
                 <h2>Tournament Results ({tournaments?.length})</h2>
+                <div>
+                    <Switch 
+                        id="hands-switcher" 
+                        name="hands-switcher" 
+                        label={toggleHandFilter ? "All tournaments" : "Tournaments with hands"}
+                        onChange={filterHands} 
+                    />
+                    <Switch 
+                        id="filter-switcher" 
+                        name="filter-switcher" 
+                        label={!toggleFilter ? "Show Filter" : "Hide Filter"}
+                        onChange={() => setToggleFilter(!toggleFilter)} 
+                    />
+                </div>
             </div>
+            <div className={`filter_list ${toggleFilter ? "active" : ""}`}>
+                   <Filters 
+                    allTournaments={tournaments} 
+                    filteredTournaments={filteredTournaments} 
+                    setFilteredTournaments={setFilteredTournaments}
+                    setSortedTournaments={setSortedTournaments}
+                    setDateFormattedTournaments={setDateFormattedTournaments}
+                    hasBuyInSlider
+                    hasEntrantsSlider
+                    />
+                </div>  
             <hr />
             <TabsManager tabs={tabs} tabsId="tournament-results">
                 <Tabs />
@@ -73,7 +119,7 @@ const ResultsPage = () => {
                     </TabPanel>
                     <TabPanel>
                         <ResultsGraph
-                            tournaments={tournaments}
+                            tournaments={filteredTournaments}
                             isLoading={isLoading}
                             rakebackData={rakebackData}
                         />
