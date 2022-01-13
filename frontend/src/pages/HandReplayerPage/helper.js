0bd169f2122e05275006d387ce85d1e374f65931
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2022-01-13 20:27:42 +0100

    Add createPlayerStory

--- frontend/src/pages/HandReplayerPage/helper.js
+++ frontend/src/pages/HandReplayerPage/helper.js
@@ -19,3 +19,51 @@ export const createStory = activeHand => {
 
     return story
 }
+
+export const createReplayerStory = (activeHand, story) => {
+    const allSeats = Object.keys(activeHand).filter(key => key.includes("seat"))
+    const initialState = allSeats.map((seat, index) => {
+        
+        if (!activeHand[seat]?.playerStack) return null
+        
+        return ({
+        ["seat_"+(index+1)]: {
+            playerSeat: "seat_"+(index+1),
+            playerHand: null,
+            playerName: activeHand[seat]?.playerName,
+            playerPosition: activeHand[seat]?.playerPosition,
+            playerStack: activeHand[seat]?.playerStack,
+            playerAnte: activeHand[seat]?.playerAnte,
+            playerSmallBlind: activeHand[seat]?.playerSmallBlind,
+            playerBigBlind: activeHand[seat]?.playerBigBlind,
+            playerAction: "",
+            playerBet: 0,
+            board: null
+        }
+    })})
+    const filteredInitialState = initialState.filter(state => state !== null)
+    let completeStory = []
+    let updatedState = filteredInitialState
+    // console.log(story)
+
+    story.forEach((chapter, index) => {
+        // add and update (enemy) hole cards
+        if (Object.keys(chapter).includes("holeCards")) Object.keys(filteredInitialState).map((seat, i) => {
+            const seatNumber = parseFloat(seat)+1
+            console.log(updatedState)
+            // if (updatedState["seat_" + seatNumber].playerName === "KeinKönich")
+        })
+
+        // add actions
+
+        // add betsizes
+
+        // add/update board
+
+        
+        // console.log(chapter)
+        completeStory.push(updatedState)
+    })
+
+    return null
+}
